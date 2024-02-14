d65ffe0189b2484e597c8647770194456a831618
sumsumcorn <nicecorntea@naver.com>
2024-02-14 15:22:24 +0900

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import ThemeButton from './ThemeButton';
 
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <div>
+      hello<Button>버튼입니다</Button>
+      <ThemeButton />
+    </div>
+  );
 }
